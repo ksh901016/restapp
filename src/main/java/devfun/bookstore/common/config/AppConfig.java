bb8e93a48c1586a24b2376f366e63c102956f779
sunghyun <ksh901016@naver.com>
2019-04-01 17:11:59 +0900

    gitignore 작성 및 Persistence Layer 계층 구현, Test Case 구현 및 테스트 완료 (Logback lib 추가 및 오류 수정)

--- src/main/java/devfun/bookstore/common/config/AppConfig.java
+++ src/main/java/devfun/bookstore/common/config/AppConfig.java
@@ -19,7 +19,8 @@ public class AppConfig {
 
     @Bean
     public DataSource dataSource(){
-        // 메모리 방시기기 때문에 애플리케이션이 실행될때 HSQLDB가 초기화되면서 메모리에 적재된다. (실행할 때마다 스키마 정보와 데이터를 적재시켜야함)
+        // 메모리 방식이기 때문에 애플리케이션이 실행될때 HSQLDB가 초기화되면서 메모리에 적재된다.
+        // (실행할 때마다 스키마 정보와 데이터를 적재시켜야함)
         return new EmbeddedDatabaseBuilder()
                 .setName("testdb") // DB 이름 설정
                 .setType(EmbeddedDatabaseType.HSQL) // DB 종류 설정
